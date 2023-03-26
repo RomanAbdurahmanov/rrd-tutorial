20fcc2fea705286a1f5a2f44496511d7af0b2bbe
Roman Abdurahmanov <abdurahmanov.roman@gmail.com>
2023-03-26 17:11:28 +0400

    Added Home and About pages, routes to them and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
-  return <div className='App'>Hello</div>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <header>
+          <nav>
+            <h1>Jobarouter</h1>
+            <NavLink to={'/'}>Home</NavLink>
+            <NavLink to={'/About'}>About</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
